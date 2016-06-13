7d059af7475f8c7d3282f647b8cfa1c6e8b645dd
Lenar Akhmadeev <lenarakhmadeev@gmail.com>
2016-06-13 23:21:16 +0500

    Add livefeedmessage.add

--- lib/bitrix24_cloud_api/CRM/additional_entities.rb
+++ lib/bitrix24_cloud_api/CRM/additional_entities.rb
@@ -1,5 +1,13 @@
 module Bitrix24CloudApi
   module CRM
+    class Livefeedmessage < Bitrix24CloudApi::Crm
+      class << self
+        def add(client, query = {})
+          client.make_post_request(resource_url(client, __method__), query)
+        end
+      end
+    end
+
     class ProductRows < Bitrix24CloudApi::Crm
       class << self
         undef_method :add, :delete, :update
